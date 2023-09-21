d3e880415b43a926a3b6d3eafbf9526590c422e5
selectjundev <selectjun.dev@gmail.com>
2023-09-21 23:06:23 +0900

    [ feat ] 상관관계 ID 필터 추가

--- src/main/java/com/attendancecheck/accountservice/AccountServiceApplication.java
+++ src/main/java/com/attendancecheck/accountservice/AccountServiceApplication.java
@@ -2,8 +2,12 @@ package com.attendancecheck.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@RefreshScope
+@EnableEurekaClient
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
